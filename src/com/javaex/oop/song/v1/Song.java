6bf4ee570ae751b5d8678236bb209f5a9ff594f1
박상수 <qkzn326@gmail.com>
2024-03-22 18:09:00 +0900

    20240322 커밋2

---
+++ src/com/javaex/oop/song/v1/Song.java
@@ -0,0 +1,18 @@
+package com.javaex.oop.song.v1;
+
+public class Song {
+	
+	private String title;
+	private String artist;
+	private String album;
+	private String composer;
+	private int year;
+	private int track;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+}
