594e6a5c8a9d40325dc2cc5e080b55ab9d5bebac
Parkku <qkzn326@gmail.com>
2024-05-03 15:39:41 +0900

    rollback

--- src/com/javaex/jdbc/ConnectionTest.java
+++ src/com/javaex/jdbc/ConnectionTest.java
@@ -7,34 +7,36 @@ import java.sql.Connection;
 // 접속 테스트
 public class ConnectionTest {
 
-	public static void main(String[] args) {
-		String dburl = "jdbc:oracle:thin@localhost:1521:xe";
-		String dbuser = "hr";
-		String dbpass = "hr";
-		
-		Connection conn = null;
-		
-		try {
-			// 드라이버 로드
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-		// 접속 문자열, 계정, 비밀번호를 이욧ㅇ 데이터베이스 접속시도.
-			conn = DriverManager.getConnection(dburl,dbuser,dbpass);
-			
-			// 데이터 베이스 접속 성공
-			System.out.println(conn);
-			System.out.println("연결 성공!");
-		} catch (ClassNotFoundException e) {
-			System.err.println("JDBC 드라이버를 로드하지 못했습니다.");
-			e.printStackTrace();
-		} catch (SQLException e) {
-			System.err.println("SQLError!");
-		} finally {
-			try{conn.close();
-			} catch (Exception e) {
-			
-		}
+    public static void main(String[] args) {
+        String dburl = "jdbc:oracle:thin:@localhost:1521:xe";
+        String dbuser = "hr";
+        String dbpass = "hr";
 
-	}
-	}
+        Connection conn = null;
+
+        try {
+            // 드라이버 로드
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            // 접속 문자열, 계정, 비밀번호를 이욧ㅇ 데이터베이스 접속시도.
+            conn = DriverManager.getConnection(dburl, dbuser, dbpass);
+
+            // 데이터 베이스 접속 성공
+            System.out.println(conn);
+            System.out.println("연결 성공!");
+        } catch (ClassNotFoundException e) {
+            System.err.println("JDBC 드라이버를 로드하지 못했습니다.");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.err.println("SQLError!");
+            e.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+            } catch (Exception e) {
+
+            }
+
+        }
+    }
 }
 
